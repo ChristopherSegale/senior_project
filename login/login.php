62bc47ef743b51bd01be795c15088796246001d8
ChristopherSegale <ccs.segale@gmail.com>
2019-10-16 01:56:46 +0000

    Adding preliminary pages for loggin faculty in.

---
+++ login/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+  <img src="/images/NetBoard_Logo.png" />
+  <h1>Login check</h1>
+  <hr />
+    <?php
+      echo "<p>Successfully logged in.</p>\n";
+    ?>
+  </body>
+</html>
+
