3d5fc89a3fc7f55b38adbef91614ab292d9bf5fc
ChristopherSegale <ccs.segale@gmail.com>
2019-10-16 02:12:24 +0000

    Adding faculty page to facilitate the insertion of new faculty members.

---
+++ faculty/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <title>Faculty Page</title>
+  </head>
+  <body>
+    <img src="/images/NetBoard_Logo.png" />
+    <h1>Faculty Page</h1>
+    <hr />
+    <?php
+      echo "<h2><a href=\"add_fac.php\"> Create Admin/Moderator</a></h2>\n";
+    ?>
+  </body>
+</html
