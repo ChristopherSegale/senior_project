5a24aa89f3600e082c6bb59ab5a9aa7bff443272
ChristopherSegale <ccs.segale@gmail.com>
2019-10-16 22:09:49 +0000

    Adding actual support to insert faculty.

---
+++ faculty/add_fac.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <title>Add Faculty</title>
+  </head>
+  <body>
+    <img src="/images/NetBoard_Logo.png" />
+    <h1>Add a new Faculty Member</h1>
+    <hr />
+    <?php
+      if (isset($_POST['inc_fac'])) {
+        $email = $_POST['email'];
+	$pass = $_POST['pass'];
+	$aom = $_POST['aom'];
+
+	if (strcmp($email, "") === 0) {
+	  echo "<p>Email field must be filled</p>\n" .
+	       "<p>Go <a href=\"add_fac.php\">back</a> to enter the value into the input field.</p>\n";
+	}
+	else {
+	  if (strcmp($pass, "") === 0) {
+	    echo "<p>Password field must be filled</p>\n" .
+	         "<p>Go <a href=\"add_fac.php\">back</a> to enter the value into the input field.</p>\n";
+	  }
+	  else {
+	    include "inc_admin_connect.php";
+	    if ($conn === FALSE) {
+	      echo "<p>Could not connect to the database. " . mysqli_error() . "</p>\n" .
+	           "<p>Go <a href=\"add_fac.php\">back</a> and try again when the server is back up.</p>\n";
+	    }
+	    else {
+	      $sql_string = "INSERT INTO admin_mods (email, password, admin_or_mod) VALUES " .
+	                    "(\"" . $email . "\", \"" . password_hash($pass, PASSWORD_DEFAULT) . "\", \"" . $aom . "\")";
+	      mysqli_select_db($conn, $database);
+	      @mysqli_query($conn, $sql_string);
+	      echo "<p>Faculty member added to database.</p>\n";
+	    }
+	  }
+	}
+      }
+      else {
+        ?>
+	<form action="add_fac.php" method="post">
+	  <p>
+	    <select name="aom">
+	      <option value="admin">Administrator</option>
+	      <option value="mod">Moderator</option>
+	    </select><br />
+	    Email Address: <input type="text" name="email" /><br />
+	    Password: <input type="password" name="pass" />
+	  </p>
+	  <input type="submit" name="inc_fac" value="Submit" />
+	</form>
+	<?php
+      }
+    ?>
+  </body>
+</html>
