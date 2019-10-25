579ff0dbcb0fd5bc2f526ff8b703a9d77acc0b62
ChristopherSegale <ccs.segale@gmail.com>
2019-10-25 15:56:22 +0000

    Untested insert thread script.

---
+++ category/insert_thread.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <title>Insert Thread</title>
+  </head>
+  <body>
+    <img src="/images/NetBoard_Logo.png" />
+    <h1>Insert Thread</h1>
+    <hr />
+    <?php
+      include "inc_user_connect.php";
+      if ($conn === FALSE) {
+        echo "<p>Could not connect to database.  Try again another time</p>\n";
+      }
+      else {
+        if (isset($_POST['insert_thread'])) {
+	  if (strcmp($_POST['title'], "") === 0) {
+	    echo "<p>Title is required to post thread.</p>\n";
+	  }
+	  else if (strcmp($_POST['op'], "") === 0) {
+	    echo "<p>Post body must be filled in to post thread.</p>\n";
+	  }
+	  else {
+	    mysqli_select_db($conn, $database);
+	    $thread_insert = "INSERT INTO thread (subject, created, category_id) VALUES " .
+	                     "(\"" . $_POST['title'] . "\", NOW(), " . $_POST['cat_id'] . ")";
+	    @mysqli_query($conn, $thread_insert);
+	    $thread_row = @mysqli_query($conn, "SELECT id WHERE title = \"" . $_POST['title'] . "\"");
+	    $thread_id = @mysqli_fetch_row($thread_row)[0];
+	    $op_insert = "INSERT INTO post (content, created, thread_id) VALUES " .
+	                 "(\" . $_POST['op'] . "\", NOW(), " . $thread_id . ")";
+	  }
+	}
+	mysqli_close($conn);
+      }
+    ?>
+  </body>
+</html>
