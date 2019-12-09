151b15fda1071c978b5be2944a6f5d8dcc3167c6
ChristopherSegale <ccs.segale@gmail.com>
2019-12-09 19:23:52 +0000

    Adding style to logout page.

--- faculty/logout.php
+++ faculty/logout.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css ">
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<script src="styles/bootsrap/js/bootstrap.min.js"></script>
     <title>Log Out</title>
   </head>
   <body>
+	<body style="background-image: url('../../images/IndexBackground.png'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%">
+
+	<div class="jumbotron text-center" style="background-color: transparent">
+		<img class="img-fluid" src="../../images/NetBoard_Logo2.png" />
+	</div>
+
+	<div class="row">
+		<div class="col-lg-8" style="padding: 10px">
+			<div class='row-center' style='border: 1px solid black; border-radius: 15px; background-color: white; width: 100%; padding: 10px'>
     <?php
       include "inc_owner_connect.php";
       if ($conn === FALSE) {
@@ -18,5 +30,8 @@
         header("location:/faculty");
       }
     ?>
+			</div>
+		</div>
+	</div>
   </body>
 </html>
