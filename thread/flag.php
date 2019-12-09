d79b6990af9c45f3ced3c9c49c6fd4b30fdbaa37
ChristopherSegale <ccs.segale@gmail.com>
2019-12-09 19:07:45 +0000

    Adding style to flagging and deletion pages.

--- thread/flag.php
+++ thread/flag.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css ">
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<script src="styles/bootsrap/js/bootstrap.min.js"></script>
     <title>Flagging Page</title>
   </head>
   <body>
-    <img src="/images/NetBoard_Logo.png" />
+	<body style="background-image: url('../../images/IndexBackground.png'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%">
+
+	<div class="jumbotron text-center" style="background-color: transparent">
+		<img class="img-fluid" src="../../images/NetBoard_Logo2.png" />
+	</div>
+
+	<div class="container" style='background-color: lightgrey; border-radius: 25px; '>
+		<div class="row bg-dark text-white" style='border-radius: 25px; '>
+			<div class="col-mdlg-8" style-"padding: 5px; ">
+
     <h1>Flagging Page</h1>
     <hr />
+			</div>
+		</div>
+
+	<div class="row">
+		<div class="col-lg-8" style="padding: 10px">
+			<div class='row-center' style='border: 1px solid black; border-radius: 15px; background-color: white; width: 100%; padding: 10px'>
+
     <?php
       date_default_timezone_set('America/New_York');
       echo "<p>" . date('m/d/Y h:i:s a', time()) . "</p>\n<hr />\n";
@@ -22,5 +41,9 @@
 	echo "<p>Post number <a href=\"/post/index.php/" . $_POST['pn'] . "\">" . $_POST['pn'] . "</a> has been flagged.</p>\n";
       }
     ?>
+			</div>
+		</div>
+	</div>
+	</div>
   </body>
 </html>
