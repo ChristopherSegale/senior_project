01f6327991e8bba9e5a92ec97052aaeb32e0fbd2
ChristopherSegale <ccs.segale@gmail.com>
2019-12-09 19:19:39 +0000

    Adding style to post redirection page.

--- post/index.php
+++ post/index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head>
-    <title>Post Redirction</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css ">
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<script src="styles/bootsrap/js/bootstrap.min.js"></script>
+    <title>Post Redirection</title>
   </head>
   <body>
-    <img src="/images/NetBoard_Logo.png" />
+	<body style="background-image: url('../../images/IndexBackground.png'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%">
+
+	<div class="jumbotron text-center" style="background-color: transparent">
+		<img class="img-fluid" src="../../images/NetBoard_Logo2.png" />
+	</div>
+
+	<div class="container" style='background-color: lightgrey; border-radius: 25px; '>
+		<div class="row bg-dark text-white" style='border-radius: 25px; '>
+			<div class="col-mdlg-8" style-"padding: 5px; ">
     <h1>Post Redirection</h1>
     <hr />
+			</div>
+		</div>
+
+	<div class="row">
+		<div class="col-lg-8" style="padding: 10px">
+			<div class='row-center' style='border: 1px solid black; border-radius: 15px; background-color: white; width: 100%; padding: 10px'>
     <?php
       include "inc_user_connect.php";
       if ($conn === FALSE) {
@@ -24,5 +41,9 @@
 	}
       }
     ?>
+			</div>
+		</div>
+	</div>
+	</div>
   </body>
 </html>
