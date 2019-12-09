13eb669a62fd9202663ee0464542b680b6d6d611
ChristopherSegale <ccs.segale@gmail.com>
2019-12-09 18:43:01 +0000

    Adding style to category insertion script.

--- create_category/insert_category.php
+++ create_category/insert_category.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css ">
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<script src="styles/bootsrap/js/bootstrap.min.js"></script>
     <title>Insert Category</title>
   </head>
   <body>
-    <img src="/images/NetBoard_Logo.png" />
+	<body style="background-image: url('../../images/IndexBackground.png'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%">
+
+	<div class="jumbotron text-center" style="background-color: transparent">
+		<img class="img-fluid" src="../../images/NetBoard_Logo2.png" />
+	</div>
+	<div class="container" style='background-color: lightgrey; border-radius: 25px; '>
+		<div class="row bg-dark text-white" style='border-radius: 25px; '>
+			<div class="col-mdlg-8" style-"padding: 5px; ">
     <p><a href="/">Home</a> | <a href="/faculty">Faculty Page</a></p>
     <hr />
+			</div>
+		</div>
+
+	<div class="row">
+		<div class="col-lg-8" style="padding: 10px">
+			<div class='row-center' style='border: 1px solid black; border-radius: 15px; background-color: white; width: 100%; padding: 10px'>
     <?php
       include "verify_fac.php";
       if (isset($_POST['insert_category'])) {
@@ -41,5 +57,9 @@
         echo "<p>Category has not been submitted to the database.</p>\n";
       }
     ?>
+			</div>
+		</div>
+	</div>
+	</div>
   </body>
 </html>
